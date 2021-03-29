7d22a750a27155c637b9b4e0e3152968f25111e7
Thana-Jaradat <2017901002@ses.yu.edu.jo>
2021-03-29 20:18:15 +0300

    added access token

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const { sequelize} = require('./models');
+const { sequelize } = require('./models');
+const User = require('./models');
 const express = require('express');
 const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
 const commentRoutes = require('./routes/commentRoutes');
@@ -14,15 +16,40 @@ async function build(){
 build();
 
 app.set('view engine','ejs');
-
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
-app.get('/' , (req , res) => {
+//to get the Homepage
+app.get('/' , async (req , res) => {
     
-    res.redirect('/posts');
-    });
+    res.json({"message": 'Welcome to the Homepage'});
+    }); 
+
+     //to generate an access token when logging in
+    function generateAccessToken(username){
+        return jwt.sign(username,'secret_key', {expiresIn: '1800s'})
+    }
+
+//login (POST)
+app.post('/' , async (req,res) => {
+    //MUST DO THE BASIC AUTHENTICATION PROCESS FIRST
+    console.log(req.body);
+    const someUser = await User.user.findOne({email:req.body.email });
+    if (someUser){
+        const token = generateAccessToken({ username: req.body.email });
+        res.json(token);
+    }
+    else
+    res.json({message: "Invalid credentials"});
+
+}); 
+
+//SIGN OUT
+app.delete('/sign-out' , (req,res) => {
+
+});
+
 app.use('/users', userRoutes);
 app.use('/posts/:postid/comments', commentRoutes);
 app.use('/posts/:postid/reactions', reactionRoutes);
